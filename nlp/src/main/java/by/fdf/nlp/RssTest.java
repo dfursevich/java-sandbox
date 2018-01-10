e525acfccb7072e8937bec6df4323815cc21ea14
Dmitry Fursevich <fursevich.dmitry@gmail.com>
2018-01-11 01:11:05 +0300

    wednesday

--- nlp/src/main/java/by/fdf/nlp/RssTest.java
+++ nlp/src/main/java/by/fdf/nlp/RssTest.java
@@ -1,6 +1,7 @@
 package by.fdf.nlp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
@@ -11,7 +12,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.core.io.ClassPathResource;
 
-import java.util.Map;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -29,14 +35,36 @@ public class RssTest implements CommandLineRunner {
         SyndFeed feed = new SyndFeedInput().build(new XmlReader(new ClassPathResource("kremlin.ru.xml").getFile()));
 
         Map<String, Integer> persons = feed.getEntries().stream()
-                .flatMap(entry ->
-                        new Document(Jsoup.parse(entry.getContents().get(0).getValue()).text()).sentences().stream()
-                                .flatMap(sentence -> sentence.tokens().stream())
-                                .filter(token -> "PERSON".equals(token.ner()))
-                                .map(Token::word)
-                                .collect(Collectors.toSet()).stream()
-                )
-                .collect(Collectors.toMap(token -> token, token -> 1, (v1, v2) -> v1 + v2));
+                .flatMap(entry -> new Document(Jsoup.parse(entry.getContents().get(0).getValue()).text()).sentences()
+                        .stream()
+                        .flatMap(sentence ->
+                                sentence.tokens().stream()
+                                        .collect(() -> Lists.<List<String>>newArrayList(Lists.<String>newArrayList()),
+                                                (list, token) -> {
+                                                    List<String> current = list.get(list.size() - 1);
+                                                    if ("PERSON".equals(token.ner())) {
+                                                        current.add(token.lemma());
+                                                    } else if (!current.isEmpty()) {
+                                                        list.add(Lists.newArrayList());
+                                                    }
+                                                }, (list1, list2) -> {
+                                                    throw new UnsupportedOperationException();
+                                                })
+                                        .stream())
+                        .filter(person -> !person.isEmpty())
+                        .map(person -> person.stream().collect(Collectors.joining(" ")))
+                        .collect(Collectors.toSet())
+                        .stream())
+                .collect(Collectors.toMap(person -> person, person -> 1, (v1, v2) -> v1 + v2))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
 
         System.out.println(new ObjectMapper().writeValueAsString(persons));
     }
